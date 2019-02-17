4a1078304fe8c63324f5ceaabea76bb16f0fba91
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2019-02-17 18:30:12 -0500

    Updates

--- precache-manifest.c61ee1fe36ad4b840e820a2b84d64490.js
+++ precache-manifest.c61ee1fe36ad4b840e820a2b84d64490.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/king-security/static/media/outline-icons.cd6c777f.woff2"
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/king-security/static/media/outline-icons.701ae6ab.eot"
   },
   {
     "revision": "8f711436f5004a9b2ba0",
@@ -12,8 +12,8 @@ self.__precacheManifest = [
     "url": "/king-security/static/js/runtime~main.1a3b9ca1.js"
   },
   {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "/king-security/static/media/icons.962a1bf3.svg"
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/king-security/static/media/brand-icons.a1a749e8.svg"
   },
   {
     "revision": "1a6a0c43ad9383471f40",
@@ -32,28 +32,28 @@ self.__precacheManifest = [
     "url": "/king-security/static/media/flags.9c74e172.png"
   },
   {
-    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
-    "url": "/king-security/static/media/icons.8e3c7f55.eot"
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/king-security/static/media/icons.0ab54153.woff2"
   },
   {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "/king-security/static/media/icons.b87b9ba5.ttf"
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/king-security/static/media/icons.8e3c7f55.eot"
   },
   {
     "revision": "faff92145777a3cbaf8e7367b4807987",
     "url": "/king-security/static/media/icons.faff9214.woff"
   },
   {
-    "revision": "0ab54153eeeca0ce03978cc463b257f7",
-    "url": "/king-security/static/media/icons.0ab54153.woff2"
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/king-security/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "8f711436f5004a9b2ba0",
     "url": "/king-security/static/js/main.8f711436.chunk.js"
   },
   {
-    "revision": "701ae6abd4719e9c2ada3535a497b341",
-    "url": "/king-security/static/media/outline-icons.701ae6ab.eot"
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/king-security/static/media/outline-icons.cd6c777f.woff2"
   },
   {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
@@ -64,16 +64,20 @@ self.__precacheManifest = [
     "url": "/king-security/static/media/outline-icons.ad97afd3.ttf"
   },
   {
-    "revision": "e8c322de9658cbeb8a774b6624167c2c",
-    "url": "/king-security/static/media/brand-icons.e8c322de.woff2"
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/king-security/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/king-security/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/king-security/static/media/brand-icons.13db00b7.eot"
   },
   {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "/king-security/static/media/outline-icons.82f60bd0.svg"
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/king-security/static/media/brand-icons.e8c322de.woff2"
   },
   {
     "revision": "a046592bac8f2fd96e994733faf3858c",
@@ -82,10 +86,6 @@ self.__precacheManifest = [
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
     "url": "/king-security/static/media/brand-icons.c5ebe0b3.ttf"
-  },
-  {
-    "revision": "a1a749e89f578a49306ec2b055c073da",
-    "url": "/king-security/static/media/brand-icons.a1a749e8.svg"
   },
   {
     "revision": "1a6a0c43ad9383471f40",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/king-security/precache-manifest.6d7c23f36141305b2e8c41515b154b1a.js"
+  "/king-security/precache-manifest.c61ee1fe36ad4b840e820a2b84d64490.js"
 );
 
 workbox.clientsClaim();
